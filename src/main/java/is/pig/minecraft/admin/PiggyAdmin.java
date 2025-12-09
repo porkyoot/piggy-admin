6ea277fefde37741aaaa024d66c837fffeb0b40f
Porkyoot <85759819+porkyoot@users.noreply.github.com>
2025-12-09 06:16:24 +0100

    First Commit

---
+++ src/main/java/is/pig/minecraft/admin/PiggyAdmin.java
@@ -0,0 +1,36 @@
+package is.pig.minecraft.admin;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PiggyAdmin implements ModInitializer {
+	public static final String MOD_ID = "piggy-admin";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		// LOGGER.info("Hello Fabric world!");
+
+		is.pig.minecraft.admin.config.PiggyServerConfig.load();
+
+		net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry.playS2C().register(
+				is.pig.minecraft.admin.network.SyncConfigPayload.TYPE,
+				is.pig.minecraft.admin.network.SyncConfigPayload.CODEC);
+
+		net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+			boolean allowCheats = is.pig.minecraft.admin.config.PiggyServerConfig.getInstance().allowCheats;
+			net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking.send(handler.getPlayer(),
+					new is.pig.minecraft.admin.network.SyncConfigPayload(allowCheats));
+		});
+	}
+}
---
+++ src/main/java/is/pig/minecraft/admin/network/PiggyNetworking.java
@@ -0,0 +1,8 @@
+package is.pig.minecraft.admin.network;
+
+import net.minecraft.resources.ResourceLocation;
+
+public class PiggyNetworking {
+    public static final ResourceLocation SYNC_CONFIG_PACKET = ResourceLocation.fromNamespaceAndPath("piggy-admin",
+            "sync_config");
+}
